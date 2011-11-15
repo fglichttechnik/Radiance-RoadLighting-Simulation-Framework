444d89574a805b050f4eaa9e205616b86180675c
Sahil Sachdeva <sahilsachdeva@mister_m_eth.(none)>
2011-11-15 16:00:27 +0100

    fix in LMK XML

--- RoadRad/src/simulator.py
+++ RoadRad/src/simulator.py
@@ -196,11 +196,11 @@ class simulator:
                         else:
                             xmax = x
                             ymax = y
-            print xmin
-            print ymin
-            print xmax
-            print ymax
-            xmlOut.write( "<LMKData>\n<dataSource src=\"out"+str(i)+".mat\" type=\"mat\"/>\n<RectObject>\n<upperLeft x=\""+str(xmax)+"\" y=\""+str(ymax)+"\"/>\n<lowerRight x=\""+str(xmin)+"\" y=\""+str(ymin)+"\"/>\n<border pixel=\"0\"/>\n<position p=\"--\"/>\n</RectObject>\n</LMKData>\n\n" )
+            xmin = xmin + 1
+            ymin = ymin + 1
+            xmax = xmax - 1
+            ymax = ymax - 1
+            xmlOut.write( "<LMKData>\n<dataSource src=\"out"+str(i)+".pf\" type=\"pf\"/>\n<RectObject>\n<upperLeft x=\""+str(xmax)+"\" y=\""+str(ymax)+"\"/>\n<lowerRight x=\""+str(xmin)+"\" y=\""+str(ymin)+"\"/>\n<border pixel=\"2\"/>\n<position p=\"--\"/>\n</RectObject>\n</LMKData>\n\n" )
         xmlOut.close()          
         return
     
