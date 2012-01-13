9a204e0b81ba3b1a153f00ddaed4e80c5ad59233
Jan Winter <j.winter@tu-berlin.de>
2012-01-13 20:10:04 +0100

    bugfix for new LMKMatSet

--- RoadRad/src/RoadRad_batch.sh
+++ RoadRad/src/RoadRad_batch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# start this shell script with ./RoadRad_batch.sh from the src directory
 
-# start this shell with ./RoadRad_batch.sh from the src directory
-
-./RoadRad.py --dir SceneX
-./RoadRad.py --dir SceneY
+./RoadRad.py --dir Treskowstr_LED_R3_1035
+./RoadRad.py --dir Treskowstr_LED_R3_1449
+./RoadRad.py --dir Treskowstr_LED_R3_2070
+./RoadRad.py --dir Treskowstr_LED_R3_3519
+./RoadRad.py --dir Treskowstr_LED_R3_4554
+./RoadRad.py --dir Treskowstr_LED_R3_7245
