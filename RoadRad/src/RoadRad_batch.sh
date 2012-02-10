61e322587556581e0a1c8f708a708e55459756b6
Sandy Buschmann <16900309523@mailbox.tu-berlin.de>
2012-02-10 10:05:53 +0100

    batch debugging

--- RoadRad/src/RoadRad_batch.sh
+++ RoadRad/src/RoadRad_batch.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 # start this shell script with ./RoadRad_batch.sh from the src directory
 
-./RoadRad.py --dir Treskowstr_LED_RP8_4_undim
-./RoadRad.py --dir Treskowstr_LED_RP8_4_ds
-./RoadRad.py --dir Treskowstr_HS_RP8_1035
-./RoadRad.py --dir Treskowstr_LED_RP8_dim
+# please add the folder names of your scenes in the following array (devided by space characters):
+dirArray=(Treskowstr_LED_RP8_4 Treskowstr_LED_RP8_6)
+
+# please add your LMK database directory:		
+dirOfDatabase="/Users/sandy/Desktop/Development/LMK/LMK_Data_evaluation/database"
+
+for((i = 0; i<${#dirArray[*]}; i++));
+do 
+	./RoadRad.py --dir ${dirArray[$i]};
+	mkdir -p $dirOfDatabase/${dirArray[$i]}
+	cd ..
+	cp -R scenes/${dirArray[$i]} $dirOfDatabase/
+	cd src/
+done
+			
+
+
