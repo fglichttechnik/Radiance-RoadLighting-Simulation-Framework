72f17d5722eec285e08faa292aafab31aa5903ca
Sandy Buschmann <16900309523@mailbox.tu-berlin.de>
2012-04-27 12:05:00 +0200

    illuminances calculated but unrealistic lum. and illum. values

--- RoadRad/src/evaluator.py
+++ RoadRad/src/evaluator.py
@@ -22,11 +22,6 @@ class evaluator:
         self.octDirSuffix = '/Octs'
         self.radDirSuffix = '/Rads'
         self.picDirSuffix = '/Pics'
-        self.ldcSuffix = '/LDCs'
-        self.LMKSetMat = '/' + os.path.basename( self.rootDirPath )#'/LMKSetMat'
-        print self.LMKSetMat
-        #sys.exit(0)
-        self.LMKSetMatFilename = '/LMKSetMat.xml'
         self.picSubDirSuffix = '/pics'
         
         self.scene = Scene.Scene
@@ -34,15 +29,10 @@ class evaluator:
         #output image resolution
         self.horizontalRes = 1380
         self.verticalRes = 1030
-        
-        path = self.rootDirPath + self.radDirPrefix
-        #print "PATH " + path
-        dirList = os.listdir( path )
                
         self.focalLength = 0
         self.verticalAngle = 0
         self.horizontalAngle = 0
-        self.lightType = ''
         self.viewPointDistance = 60 #according to DIN EN 13201-3     
         self.viewPointHeight = 1.5
         
@@ -54,7 +44,8 @@ class evaluator:
         self.parseConfig( )
         self.makeOct( )
         self.calcLuminances( )
-        self.makePic( )
+        self.calcIlluminances( )
+        #self.makePic( )
 
         return
     
@@ -70,15 +61,7 @@ class evaluator:
             self.scene.NumLanes = int( roadDesc[0].attributes["NumLanes"].value )
             self.scene.NumPoleFields = float( roadDesc[0].attributes["NumPoleFields"].value )
             self.scene.LaneWidth = float( roadDesc[0].attributes["LaneWidth"].value )
-            self.scene.SidewalkWidth = float( roadDesc[0].attributes["SidewalkWidth"].value )
         
-        LDCDesc = dom.getElementsByTagName( 'LDC' )
-        for LDCEntry in LDCDesc:
-            if( LDCEntry.attributes ):
-                tempLDC = LDC.LDC( )
-                tempLDC.LDCName = LDCEntry.attributes["Name"].value
-                tempLDC.LDCLightSource = LDCEntry.attributes["LightSource"].value
-                self.lights.append(tempLDC)
             
         poleDesc = dom.getElementsByTagName( 'Poles' )
         for pole in poleDesc[0].childNodes:
@@ -105,22 +88,8 @@ class evaluator:
             self.focalLength = float( focalLen[ 0 ].attributes["FL"].value )
             self.calcOpeningAngle( )
                 
-            
-        #according to DIN EN 13201
-        print "pole spacing: " + str( self.Poles[0].PoleSpacing )
-        self.measFieldLength = self.Poles[0].PoleSpacing * self.scene.NumPoleFields;   
-        
-        self.numberOfMeasurementPoints = 10
-        if( self.Poles[0].PoleSpacing > 30 ):        	
-        	while ( self.Poles[0].PoleSpacing / self.numberOfMeasurementPoints ) > 3:
-        		self.numberOfMeasurementPoints = self.numberOfMeasurementPoints + 1
-        
-        print "numberOfMeasurementPoints: " + str( self.numberOfMeasurementPoints )
-        self.measurementStepWidth = self.measFieldLength / self.numberOfMeasurementPoints        
-        print "measurementStepWidth: " + str( self.measurementStepWidth ) 
-        print "measFieldLength: " + str( self.measFieldLength ) 
-        
-        print 'Sucessfully Parsed.'
+        print 'Sucessfully Parsed.'        
+       
 
     #calculate the horizontal and vertical opening angle of the camera required for the rendering
     def calcOpeningAngle( self ):
@@ -141,42 +110,110 @@ class evaluator:
     
     #Prints view point files for every lane
     #Based on the viewpoint mode, one of several viewpoints are written
-    def calcLuminances( self ):
-    	directionZ = 0 - self.viewPointHeight
-    	print 'Generating: luminance view directions'
-        self.rows = []   
+    def calcLuminances( self ):    
+		
+    	print 'Generating: luminance values according to DIN EN 13201-3'
+		
+    	#according to DIN EN 13201
+        print "	pole spacing: " + str( self.Poles[0].PoleSpacing )
+        self.measFieldLength = self.Poles[0].PoleSpacing * self.scene.NumPoleFields   
+        
+        self.numberOfMeasurementPoints = 10
+        if( self.Poles[0].PoleSpacing > 30 ):        	
+        	while ( self.Poles[0].PoleSpacing / self.numberOfMeasurementPoints ) > 3:
+        		self.numberOfMeasurementPoints = self.numberOfMeasurementPoints + 1
+        
+        print "	number of measurement points: " + str( self.numberOfMeasurementPoints )
+        self.measurementStepWidth = self.measFieldLength / self.numberOfMeasurementPoints        
+        print "	measurement step width: " + str( self.measurementStepWidth ) 
+        print "	measurment field length: " + str( self.measFieldLength ) 
+    
+    	directionZ = self.viewPointHeight   
         f = open( self.workingDirPath + self.radDirPrefix + '/luminanceCoordinates.pos', "w" )
         
     	for laneNumber in range( self.scene.NumLanes ):    		        
 			#
-        	print "lane index: " + str( laneNumber )
+        	print "	lane: " + str( laneNumber + 1 )
         	#
         	#calc view direction according to DIN standard observer
         	viewerXPosition = ( self.scene.LaneWidth * (laneNumber + 0.5 ) )
-        	print 'viewer X position: ' + str( viewerXPosition )
+        	print '		viewer X position: ' + str( viewerXPosition )
         	viewPoint = ' {0} {1} {2}'.format( viewerXPosition, self.viewPointDistance, self.viewPointHeight )
         	distanceOfMeasRows = self.scene.LaneWidth / 3
         	#
         	for rowNumber in range( 3 ):        		
         		rowXPosition = self.scene.LaneWidth * (laneNumber + ( ( rowNumber + 1 ) * 0.25 ) )
-        		print 'row x position: ' + str( rowXPosition )
-        		directionX = rowXPosition - viewerXPosition
+        		print '		row x position: ' + str( rowXPosition )
+        		directionX = viewerXPosition -rowXPosition
         		#
         		for measPointNumber in range( self.numberOfMeasurementPoints ):
-        			directionY = ( ( measPointNumber + 0.5 ) * self.measurementStepWidth ) + self.viewPointDistance
+        			directionY = self.viewPointDistance - ( ( measPointNumber + 0.5 ) * self.measurementStepWidth )
         			viewDirection = ' {0} {1} {2}'.format( directionX, directionY, directionZ )
-        			print ' ' + str( viewPoint ) + str( viewDirection ) + '\n'
         			f.write( str( viewPoint ) + str( viewDirection ) + '\n')
                 	
                 	
         f.close( )
         
-        print 'Calculating: Luminances for defined rays'
         cmd = "rtrace -h -oo -od -ov /Users/sandy/Desktop/Development/RoadRad/RoadRad/scenes/Treskowstr_LED_RP8_4/Octs/scene_din.oct < " + self.workingDirPath + self.radDirPrefix + "/luminanceCoordinates.pos  | rcalc -e '$1=179*($1*.265+$2*.67+$3*.065)' > " + self.workingDirPath + self.radDirPrefix + "/rawLuminances.txt"
         os.system( cmd )
         cmd = "rlam " + self.workingDirPath + self.radDirPrefix + "/luminanceCoordinates.pos " + self.workingDirPath + self.radDirPrefix + "/rawLuminances.txt > " + self.workingDirPath + self.radDirPrefix + "/luminances.txt"
         os.system( cmd )
+        print "Luminance values successfully calculated"
         
+    #Prints view point files for every lane
+    #Based on the viewpoint mode, one of several viewpoints are written
+    def calcIlluminances( self ):    
+		
+    	print 'Generating: illuminance values according to DIN EN 13201-3'
+		
+    	#according to DIN EN 13201
+        print "	pole spacing: " + str( self.Poles[0].PoleSpacing )
+        self.measFieldLength = self.Poles[0].PoleSpacing * self.scene.NumPoleFields   
+        
+        self.numberOfMeasurementPoints = 10
+        if( self.Poles[0].PoleSpacing > 30 ):        	
+        	while ( self.Poles[0].PoleSpacing / self.numberOfMeasurementPoints ) > 3:
+        		self.numberOfMeasurementPoints = self.numberOfMeasurementPoints + 1
+        
+        print "	number of measurement points: " + str( self.numberOfMeasurementPoints )
+        self.measurementStepWidth = self.measFieldLength / self.numberOfMeasurementPoints        
+        print "	measurement step width: " + str( self.measurementStepWidth ) 
+        print "	measurment field length: " + str( self.measFieldLength ) 
+    
+    	viewDirection = '0 0 -1'
+    	positionZ = '0.02'
+    	
+    	self.numberOfMeasurementRows = 3
+    	while ( self.scene.LaneWidth / self.numberOfMeasurementRows ) > 1.5:
+    		self.numberOfMeasurementRows = self.numberOfMeasurementRows + 1
+    		
+    	print "	number of measurement rows per lane: " + str( self.numberOfMeasurementRows )
+    	
+    	f = open( self.workingDirPath + self.radDirPrefix + '/illuminanceCoordinates.pos', "w" )
+    	
+    	for laneNumber in range( self.scene.NumLanes ):
+    		#
+    		print "	lane: " + str( laneNumber +1 )
+    		#
+    		for rowNumber in range( self.numberOfMeasurementRows ):
+    			positionX = self.scene.LaneWidth * (laneNumber + ( ( rowNumber + 1 ) * 0.25 ) )
+    			print "		position X: " + str( positionX )
+    			#
+    			for measPointNumber in range( self.numberOfMeasurementPoints ):
+    				positionY = ( ( measPointNumber + 0.5 ) * self.measurementStepWidth )
+    				viewPoint = '{} {} {} '.format( positionX, positionY, positionZ )
+    				print '			' + str(viewPoint ) + str( viewDirection )
+    				f.write( str(viewPoint ) + str( viewDirection ) + '\n' )
+    	             	
+                	
+        f.close( )
+        
+        cmd1 = "rtrace -h -I /Users/sandy/Desktop/Development/RoadRad/RoadRad/scenes/Treskowstr_LED_RP8_4/Octs/scene_din.oct < " + self.workingDirPath + self.radDirPrefix + "/illuminanceCoordinates.pos  | rcalc -e '$1=179*($1*.265+$2*.67+$3*.065)' > " + self.workingDirPath + self.radDirPrefix + "/rawIlluminances.txt"
+        os.system( cmd1 )
+        cmd2 = "rlam " + self.workingDirPath + self.radDirPrefix + "/illuminanceCoordinates.pos " + self.workingDirPath + self.radDirPrefix + "/rawIlluminances.txt > " + self.workingDirPath + self.radDirPrefix + "/illuminances.txt"
+        os.system( cmd2 )
+        print "Illuminance values successfully calculated"
+    
     
     #System call to radiance framework for the actual rendering of the images
     def makePic(self):
