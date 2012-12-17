312d71c0dfdf29d6dad13991376478447d9ca433
Robert Franke <robertfranke@gmx.de>
2012-12-17 11:34:17 +0100

    new classes

---
+++ RoadRad/src/Classes/Poles.py
@@ -0,0 +1,33 @@
+# This Python file uses the following encoding: utf-8
+import Pole as modulPole
+from lxml import etree
+
+class Poles:
+    def __init__( self, root ):
+        self.poles = []    
+        
+        #load pole array or single pole
+        self.loadPoles( root )
+        
+    def loadPoles( self, root ):
+        poleDesc = root.find( 'Poles' )
+
+        for poleEntry in poleDesc:
+            if( poleEntry.tag == 'PoleSingle' ):
+                pole = modulPole.Pole( True )
+                pole.positionX = float( poleEntry.get( 'PositionY' ) )
+
+            else:
+                pole = modulPole.Pole( False )
+                pole.spacing = float( poleEntry.get( 'Spacing' ) )
+                isStaggered = poleEntry.get( 'IsStaggered' )
+                if isStaggered == 'False':
+                    pole.IsStaggered = False
+            
+            pole.side = poleEntry.get( 'Side' )
+            pole.height = float( poleEntry.get( 'Height' ) )
+            pole.lidc = poleEntry.get( 'LIDC' )
+            pole.overhang = float( poleEntry.get( 'Overhang' ) )
+            self.poles.append( pole )
+        
+        print '    headlights loaded ...' 
