89e92c48244d56e17a05625c3ca83910d0543387
Robert Franke <robertfranke@gmx.de>
2013-01-07 14:18:03 +0100

    bugfixes capital letters in data files

--- RoadRad/src/RoadRad_batch.sh
+++ RoadRad/src/RoadRad_batch.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 # start this shell script with ./RoadRad_batch.sh from the src directory
-
-#./RoadRad.py --dir Tr4
-# ./RoadRad.py --dir Treskowstr_LED_RP8_4_undim
-# ./RoadRad.py --dir Treskowstr_LED_RP8_4_ds
-# ./RoadRad.py --dir Treskowstr_LED_RP8_4_dim
-# ./RoadRad.py --dir Treskowstr_LED_RP8_undim
-# ./RoadRad.py --dir Treskowstr_LED_RP8_ds
-# ./RoadRad.py --dir Treskowstr_LED_RP8_dim
-# ./RoadRad.py --dir Treskowstr_HS_RP8_1035
-# ./RoadRad.py --dir Treskowstr_HS_RP8_1449
-# ./RoadRad.py --dir Treskowstr_HS_RP8_2070
-# ./RoadRad.py --dir Treskowstr_LED_RP8_1035
-# ./RoadRad.py --dir Treskowstr_LED_RP8_1449
-# ./RoadRad.py --dir Treskowstr_LED_RP8_2070
-#./RoadRad.py --dir Schade_Optimierung_E
-#./RoadRad.py --dir Schade_Optimierung_L
-
 # please add the folder names of your scenes in the following array (devided by space characters):
 dirArray=(Tr4)
 
