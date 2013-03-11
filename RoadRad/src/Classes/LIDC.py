ed50bbbff79c1fecbac0e9d795b3d0f27180d745
Robert <robertfranke@gmx.de>
2013-03-11 16:05:50 +0100

    cleaning dir

--- RoadRad/src/Classes/LIDC.py
+++
@@ -1,7 +0,0 @@
-class LIDC:
-
-    def __init__( self ):
-         self.name = "" 
-         self.lightSource = "" 
-         self.lightLossFactor = 0
-         self.spRatio = 0
--- RoadRad/src/Classes/TargetParameters.py
+++
@@ -1,7 +0,0 @@
-import Target as modulTarget
-import ViewPoint as modulViewPoint
-
-class TargetParameters:
-    def __init__( self, root ):
-        self.viewPoint = modulViewPoint.ViewPoint( root )
-        self.target = modulTarget.Target( root )
